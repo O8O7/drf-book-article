11410a067c03d562f507ca648a8ce38e47fbd791
Sakamoto-Shun <sakamoto.0514@icloud.com>
2026-03-09 17:55:16 +0900

    念の為Secretsにコメント追加

--- sample_config/settings.py
+++ sample_config/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# サンプルコードのため以下のSecretsは使用せずに、本番では環境変数から読み込むようにしてください。
 SECRET_KEY = 'django-insecure-k06p!9#gl*#34nym^k@jsfgh$u1(_v%0xha%yw65tqn)+l1i5f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
